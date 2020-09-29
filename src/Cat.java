4376c6356f8ddf8a63d9a5beb3833830f62142f7
Jonathan Woolf <jlwoolf@mymail.mines.edu>
2020-09-29 10:43:57 -0600

    Changes to be committed: 	modified:   src/Cat.java 	Centered the eyes in the head and made them the same color.

--- src/Cat.java
+++ src/Cat.java
@@ -9,7 +9,7 @@ public class Cat {
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
@@ -28,13 +28,13 @@ public class Cat {
 		int y=catY;
 		// Draw the head
 		g2.setColor(Color.black);
-		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		g2.fillOval(x, y, HEAD_DIMENSION, (int)(HEAD_DIMENSION/1.1));
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.cyan);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-		x += EYE_SEPARATION;
+		x += EYE_SEPARATION - EYE_WIDTH;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
 		g2.setColor(Color.pink);
@@ -42,6 +42,7 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
+		
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
